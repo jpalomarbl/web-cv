0013bfda4cc5a53ef4767b3c0a09cfbb6b204337
Simeón <simeon.palomar@ipla.csic.es>
2026-06-23 13:05:14 +0200

    Build test 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  site: 'https://jpalomarbl.github.io',
+  base: '/web-cv',
+})
+
